ee6d863b4af05e87699adc01404a0debd650a08d
@LarissahSut <larissah.7@gmail.com>
2022-11-22 11:23:51 -0300

    Tela logoin + inicio js

---
+++ login.js
@@ -0,0 +1,11 @@
+/* variáveis */
+
+function logar(){
+  const login = document.getElementById('login').value;
+  const password = document.getElementById('password').value;
+
+  if(login == "admin" && senha == "admin"){
+    alert("sucesso");
+  }
+  
+}
