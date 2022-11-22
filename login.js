3b4dc2e0abd21ea464abe6cd6dd95a53a308ec03
@LarissahSut <larissah.7@gmail.com>
2022-11-22 11:39:48 -0300

    Funcao js de login e redirecionamento para home

--- login.js
+++ login.js
@@ -1,11 +1,13 @@
-/* variáveis */
 
 function logar(){
   const login = document.getElementById('login').value;
   const password = document.getElementById('password').value;
 
-  if(login == "admin" && senha == "admin"){
+  if(login == "admin" && password == "admin"){
     alert("sucesso");
+    location.href = "home.html";
+  } else{
+    alert('Usuário ou senha incorretos')
   }
   
 }
