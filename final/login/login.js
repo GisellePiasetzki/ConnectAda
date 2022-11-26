84c49e984266c9f332989314e1ff2bedd50756be
Gabriela Ceranto <gabiceranto@gmail.com>
2022-11-26 17:11:12 -0300

    Função logar

--- final/login/login.js
+++ final/login/login.js
@@ -1,40 +1,25 @@
-// import { dataBase } from "../dataBase/data.js";
+import { dataBase } from "../dataBase/data.js";
 
-function logar(){
+function logar(event){
+
+  event.preventDefault();
+
 
   const login = document.getElementById("login").value;
   const password = document.getElementById("password").value;
-
-  // console.log('teste')
-
-  // const login = 'admin'
-  // const password = 'admin'
-  console.log(login)
-  console.log(password)
-
-  // if(login == "admin" && password == "admin"){
-  //   alert("sucesso");
-  //   // location.href = "/front-Gabi/feed.html";
-  // } else{
-  //   alert('Usuário ou senha incorretos')
-  // }
   
-//   dataBase.forEach((element) => {
-//     console.log(element.email,element.password)
-//     if(element.email == login && element.password == password){
-//       alert("sucesso");
-//       // location.href = "../feed/feed.html";
-//     } else {
-//       alert('Usuário ou senha incorretos')
-//     }
 
-// })
-  // return false
+  const validar = dataBase.find((element) => element.email == login && element.password == password);
+
+  if (validar){
+     alert("sucesso");
+      location.href = "../feed/feed.html";
+      return;
+  } else  alert('Usuário ou senha incorretos')
+
+
 };
 
-//console.log(dataBase[0].email)
+const buttonLogin = document.getElementById('buttonLogin')
+buttonLogin.addEventListener("click", function(event){logar(event)});
 
-const botao = document.getElementById('botaogo')
-botao.addEventListener("click", function(){logar()});
-
-// logar();
